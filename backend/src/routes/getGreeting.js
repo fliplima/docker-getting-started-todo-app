2135ffe2f68974843168a16493d837ad854fe2e8
Felipe Lima <felipe.araujolima@ufpe.br>
2025-12-31 10:33:35 -0300

    Atualizações de teste

--- backend/src/routes/getGreeting.js
+++ backend/src/routes/getGreeting.js
@@ -1,7 +1,12 @@
-const GREETING = 'Hello world!';
+const GREETINGS = [
+    'Hello world!',
+    "Whalecome!",
+    "All hands on deck!",
+    "Charting the course ahead!"
+];
 
 module.exports = async (req, res) => {
     res.send({
-        greeting: GREETING,
+        greeting: GREETINGS[ Math.floor( Math.random() * GREETINGS.length )],
     });
 };
